b9c903c2a9477419822823fa75273a454ce76cc1
lamle <lamle@gmx.com>
2016-06-02 23:12:57 +0700

    Implement play, pause and stop timer for Stop Watch

--- stopwatch/stopwatch/ViewController.swift
+++ stopwatch/stopwatch/ViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = NSTimer()
+    var count = 0
+    
+    @IBOutlet var time: UILabel!
+    
+    func updateTime(){
+        count += 1
+        time.text = "\(count)"
+    }
+    
+    @IBAction func pause(sender: AnyObject) {
+        print("Clicked pause button")
+        timer.invalidate()
+    }
+    
+    @IBAction func stop(sender: AnyObject) {
+        timer.invalidate()
+        count = 0
+        time.text = "0"
+    }
+    
+    @IBAction func play(sender: AnyObject) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(ViewController.updateTime), userInfo: nil, repeats: true)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
